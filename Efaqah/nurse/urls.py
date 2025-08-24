6a1b18751e92c401e480a77fe4a7ad597a84b671
Roba Abdullah <roba3bdullah@hotmail.com>
2025-08-24 10:40:39 +0300

    Nurse Features

--- Efaqah/nurse/urls.py
+++ Efaqah/nurse/urls.py
@@ -1,7 +1,11 @@
-from . import views
 from django.urls import path
+from . import views
 
 app_name = "nurse"
 
 urlpatterns = [
+    path('', views.nurse_dashboard, name="nurse_dashboard"),
+    path('add/', views.add_patient_view, name="add_patient_view"),
+    path('patient/<int:pk>/', views.view_patient, name='view_patient'),
+    path("update/<int:patient_id>/", views.update_patient_view, name="update_patient_view"),
 ]
